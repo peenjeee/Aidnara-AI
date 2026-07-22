ce0cb641e8ec6ea80d20cdb2d13487b71a118203
peenjeee <gastwocls02@gmail.com>
2026-07-22 15:40:05 +0700

    feat: initialize frontend project with SolidStart, Vite, and Tailwind CSS configuration

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { nitro } from "nitro/vite";
-
 import { solidStart } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [solidStart(),
+  plugins: [
+    tailwindcss(),
+    solidStart(),
     nitro()
   ]
 });
